f4b53151e74011bf20f065be376ee5de5f1549d7
gonzaliaga <gonzaliaga@gmail.com>
2024-04-25 21:25:37 -0400

    Primera clase

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const miTitulo = document.querySelector('h1');
+miTitulo.textContent = 'La Página de Irene Victoria'; 
+
+const miSubtitulo = document.querySelector('h2');
+miSubtitulo.textContent = 'Bienvenidos a mi página web';
+
+const miParrafo = document.querySelector('p');
+miParrafo.textContent = 'Esta página web es un proyecto de aprendizaje de HTML, CSS y JavaScript.';
