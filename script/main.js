96c581aef56493827192546fa70c7c1516208fd4
gonzaliaga <gonzaliaga@gmail.com>
2024-04-25 21:27:39 -0400

    agregamos un boton

--- script/main.js
+++ script/main.js
@@ -6,3 +6,8 @@ miSubtitulo.textContent = 'Bienvenidos a mi página web';
 
 const miParrafo = document.querySelector('p');
 miParrafo.textContent = 'Esta página web es un proyecto de aprendizaje de HTML, CSS y JavaScript.';
+
+const
+    miBoton = document.querySelector('button');
+    miImagen = document.querySelector('img');
+    
